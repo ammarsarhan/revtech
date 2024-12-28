24289415732a1dbb8594c96978ad59053cbd74fe
ammarsarhan <ammarsarhan06@gmail.com>
2024-12-28 13:11:12 +0200

    Set up preliminary react-router logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./views/Home"
+
 export default function App() {
   return (
-    <>
-
-    </>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </div>
   )
 }
